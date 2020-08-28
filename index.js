b7df467b9a249df57d0d587cb3120e1ed3379e29
ShantiLund <shanti.cs15@iba-suk.edu.pk>
2020-08-28 04:13:54 -0700

    m

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ io.sockets.on(ON_CONNECT, function (socket) {
                 socket.username = username;
                 userSocket[socket.username] = socket.id;
           
-                socket.broadcast.emit("broadcast", {
-                  message: username + " Logged In"
-                });
+                socket.broadcast.emit("broadcast", username);
 	// userSocket.on(EVENT_SEND_MESSAGE, function (chat_message) {
         //         userSocket.broadcast.emit(EVENT_RECEIVE_MESSAGE, chat_message);
         // });
